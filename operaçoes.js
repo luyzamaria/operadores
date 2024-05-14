51b5ab26faa78d2ea6d7adef12b474f49d4c9e56
luyzamaria <128808623+luyzamaria@users.noreply.github.com>
2024-05-14 08:19:14 -0300

    Add files via upload

---
+++ operaçoes.js
@@ -0,0 +1,21 @@
+
+const primeiroNumber = 1
+const segundoNumber = 4;
+
+const operaçãoMatemática = primeiroNumber + segundoNumber;
+
+
+const primeirNumber = `1`
+const segundNumber = `3`
+
+console.log 
+operacaoMatematica =primeiroNumber + segundNumber;
+
+console.log(operacaoMatematica)
+
+const primeiNumber = 1
+const segunNumber = `1`
+
+const operacaoMatematica = primeiroNumber + segundNumber;
+
+console.log(operacaoMatematica)
